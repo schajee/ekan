0bf626162b9a9875f167f6d3d02d9f15b1aed3c9
Shaji Ahmed <ahmeds@alphaxds.com>
2019-03-16 17:08:41 +0500

    API Search and Dataset views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from api.serializers import UserSerializer, GroupSerializer
+from api import serializers
+from web.models import Dataset
+from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -8,7 +12,7 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -16,4 +20,19 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    serializer_class = serializers.GroupSerializer
+
+
+class DatasetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    serializer_class = serializers.DatasetSerializer
+    queryset = Dataset.objects.all()
+
+    def get_queryset(self):
+        queryset = Dataset.objects.all()
+        q = self.request.query_params.get('q', None)
+        if q is not None:
+            queryset = queryset.filter(title__contains=q)
+        return queryset
