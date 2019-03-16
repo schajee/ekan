33858a58de1ab1bef9d077c6380d695a2fb2cc92
Shaji Ahmed <ahmeds@alphaxds.com>
2019-03-16 17:08:41 +0500

    API Search and Dataset views

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User, Group
+from web.models import Dataset
 from rest_framework import serializers
 
 
@@ -12,3 +13,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+
+class DatasetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Dataset
+        fields = ('title', 'slug')
